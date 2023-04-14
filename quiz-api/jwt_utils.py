1cba89be2ebdfb3715d189c0169701185e32622c
dasilvar <remi.dasilva@edu.esiee.fr>
2023-04-14 11:02:41 +0200

    feat: add jwt

---
+++ quiz-api/jwt_utils.py
@@ -0,0 +1,52 @@
+import jwt
+import datetime
+from werkzeug.exceptions import Unauthorized
+
+
+class JwtError(Exception):
+    """Exception raised for jwt errors in the quiz app 
+    """
+
+    def __init__(self, message="Jwt error"):
+        self.message = message
+        super().__init__(self.message)
+
+
+secret = "CHANGE ME"
+expiration_in_seconds = 3600
+
+
+def build_token():
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expiration_in_seconds),
+            'iat': datetime.datetime.utcnow(),
+            'sub': 'quiz-app-admin'
+        }
+        return jwt.encode(
+            payload,
+            secret,
+            algorithm="HS256"
+        )
+    except Exception as e:
+        return e
+
+
+def decode_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, secret, algorithms="HS256")
+        # if decoding did not fail, this means we are correctly logged in
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        raise JwtError('Signature expired. Please log in again.')
+    except jwt.InvalidTokenError as e:
+        raise JwtError('Invalid token. Please log in again.')
