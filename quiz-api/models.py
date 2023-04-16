f31a3d64a9a6a8374786c1141f162dcc05cd2996
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2023-04-16 17:25:10 +0200

    refact: inheritance

--- quiz-api/models.py
+++ quiz-api/models.py
@@ -1,6 +1,6 @@
 from utils import ConnectionManager
 
-class Question(ConnectionManager):
+class Question():
     def __init__(self, id_question: int, title: str, text: str, image: str, position: int) -> None:
         self.id_question = id_question
         self.title = title
@@ -11,11 +11,11 @@ class Question(ConnectionManager):
     def save(self):
         if self.id_question is None:
             query = "INSERT INTO question (title, text, image, position) VALUES (?, ?, ?, ?)"
-            result = self.execute(query, self.title, self.text, self.image, self.position)
+            result = ConnectionManager().execute(query, self.title, self.text, self.image, self.position)
             self.id_question = result.lastrowid
         else:
             query = "UPDATE question SET title=?, text=?, image=?, position=? WHERE id_question=?"
-            self.execute(query, self.title, self.text, self.image, self.position, self.id_question)
+            ConnectionManager().execute(query, self.title, self.text, self.image, self.position, self.id_question)
 
     @staticmethod
     def get_by_id(id_question) -> 'Question':
@@ -33,7 +33,7 @@ class Question(ConnectionManager):
 
     def to_json(self) -> dict:
         return {
-            "id_question": self.id_question,
+            "id": self.id_question,
             "title": self.title,
             "text": self.text,
             "image": self.image,
@@ -47,7 +47,7 @@ class Question(ConnectionManager):
     # finir R & D
 
 
-class PossibleAnswer(ConnectionManager):
+class PossibleAnswer():
     def __init__(self, id_possible_answer: int, text: str, isCorrect: bool, id_question: int) -> None:
         self.id_possible_answer = id_possible_answer
         self.text = text
@@ -57,15 +57,15 @@ class PossibleAnswer(ConnectionManager):
     def save(self):
         if self.id_possible_answer is None:
             query = "INSERT INTO possibleAnswer (text, isCorrect, id_question) VALUES (?, ?, ?)"
-            result = self.execute(query, self.text, self.isCorrect, self.id_question)
+            result = ConnectionManager().execute(query, self.text, self.isCorrect, self.id_question)
             self.id_possible_answer = result.lastrowid
         else:
             query = "UPDATE possibleAnswer SET text=?, isCorrect=?, id_question=? WHERE id_possible_answer=?"
-            self.execute(query, self.text, self.isCorrect, self.id_question, self.id_possible_answer)
+            ConnectionManager().execute(query, self.text, self.isCorrect, self.id_question, self.id_possible_answer)
 
     def to_json(self) -> dict:
         return {
-            "id_possible_answer": self.id_possible_answer,
+            "id": self.id_possible_answer,
             "text": self.text,
             "isCorrect": self.isCorrect,
             "id_question": self.id_question
