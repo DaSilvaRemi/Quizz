8ac1890636a368d07995e1e6dd59bcd421ae0ce1
catherinegaochangdffeef <catherinegaochang@gmail.com>
2023-04-22 16:27:28 +0200

    ajoute la classe Admin et Player

--- quiz-api/models.py
+++ quiz-api/models.py
@@ -14,11 +14,13 @@ class Question():
     def save(self) -> None:
         if self.id_question is None:
             query = "INSERT INTO question (title, text, image, position) VALUES (?, ?, ?, ?)"
-            result = ConnectionManager().execute(query, self.title, self.text, self.image, self.position)
+            result = ConnectionManager().execute(
+                query, self.title, self.text, self.image, self.position)
             self.id_question = result.lastrowid
         else:
             query = "UPDATE question SET title=?, text=?, image=?, position=? WHERE id_question=?"
-            ConnectionManager().execute(query, self.title, self.text, self.image, self.position, self.id_question)
+            ConnectionManager().execute(query, self.title, self.text,
+                                        self.image, self.position, self.id_question)
 
         for possible_answer in self.possible_answers:
             possible_answer.id_question = self.id_question
@@ -83,6 +85,7 @@ class Question():
 
     # finir R & D
 
+
 class PossibleAnswer():
     def __init__(self, id_possible_answer: int, text: str, isCorrect: bool, id_question: int) -> None:
         self.id_possible_answer = id_possible_answer
@@ -93,11 +96,13 @@ class PossibleAnswer():
     def save(self) -> None:
         if self.id_possible_answer is None:
             query = "INSERT INTO possibleAnswer (text, isCorrect, id_question) VALUES (?, ?, ?)"
-            result = ConnectionManager().execute(query, self.text, self.isCorrect, self.id_question)
+            result = ConnectionManager().execute(
+                query, self.text, self.isCorrect, self.id_question)
             self.id_possible_answer = result.lastrowid
         else:
             query = "UPDATE possibleAnswer SET text=?, isCorrect=?, id_question=? WHERE id_possible_answer=?"
-            ConnectionManager().execute(query, self.text, self.isCorrect, self.id_question, self.id_possible_answer)
+            ConnectionManager().execute(query, self.text, self.isCorrect,
+                                        self.id_question, self.id_possible_answer)
 
     def delete(self) -> None:
         query = "DELETE FROM possibleAnswer WHERE id_possible_answer=?"
@@ -129,7 +134,8 @@ class PossibleAnswer():
 
         for element in elements:
             id, text, isCorrect, id_question = element
-            possiblesAnswers.append(PossibleAnswer(id, text, isCorrect, id_question))
+            possiblesAnswers.append(PossibleAnswer(
+                id, text, isCorrect, id_question))
 
         return possiblesAnswers
 
@@ -147,7 +153,8 @@ class PossibleAnswer():
     @staticmethod
     def from_json(json: dict) -> 'PossibleAnswer':
         return PossibleAnswer(json.get("id", None), json['text'], json['isCorrect'], json.get("id_question", None))
-    
+
+
 class Participation:
     def __init__(self, id_player, id_question) -> None:
         self.id_player = id_player
@@ -157,7 +164,7 @@ class Participation:
         if self.id_player is not None and self.id_question is not None:
             query = "INSERT INTO participation (id_player, id_question) VALUES (?, ?)"
             ConnectionManager().execute(query, self.id_player, self.id_question)
-        elif self.id_player is not None and self.id_question is None :
+        elif self.id_player is not None and self.id_question is None:
             query = "UPDATE participation SET id_question=? WHERE id_player=?"
             ConnectionManager().execute(query, self.id_question, self.id_player)
         else:
@@ -177,7 +184,7 @@ class Participation:
         ConnectionManager().execute(query, self.id_question)
 
     @staticmethod
-    def getByIdPlayerAndQuestion(id_player : int, id_question: int) -> 'Participation':
+    def getByIdPlayerAndQuestion(id_player: int, id_question: int) -> 'Participation':
         connexionManager = ConnectionManager()
         query = "SELECT * FROM participation WHERE id_player=? AND id_question=?"
         query_result = connexionManager.execute(query, id_player, id_question)
@@ -187,8 +194,8 @@ class Participation:
             return None
 
         return Participation(elements[0], elements[1])
-    
-    def getByIdPlayer(id_player : int) -> list['Participation']:
+
+    def getByIdPlayer(id_player: int) -> list['Participation']:
         connexionManager = ConnectionManager()
         query = "SELECT * FROM participation WHERE id_player=?"
         query_result = connexionManager.execute(query, id_player)
@@ -196,7 +203,7 @@ class Participation:
 
         if elements is None:
             return None
-        
+
         participations = []
 
         for element in elements:
@@ -221,7 +228,6 @@ class Participation:
             id_player, id_question = element
             participations.append(Participation(id_player, id_question))
 
-
         return participations
 
     def to_json(self) -> dict:
@@ -236,3 +242,105 @@ class Participation:
     @staticmethod
     def from_json(json: dict) -> 'Participation':
         return Participation(json.get("id_player", None), json.get("id_question", None))
+
+# Admin
+    class Admin():
+        def __init__(self, id_admin: int, password: str) -> None:
+            self.id_admin = id_admin
+            self.password = password
+
+        def save(self) -> None:
+            if self.id_admin is None:
+                query = "INSERT INTO admin(password) VALUES (?)"
+                result = ConnectionManager().execute(query, self.password)
+                self.id_admin = result.lastrowid
+            else:
+                query = "UPDATE admin SET password = ? WHERE id_admin = ?"
+                ConnectionManager().execute(query, self.password, self.id_admin)
+
+        def delete(self) -> None:
+            query = "DELETE FROM admin WHERE id_admin = ? "
+            ConnectionManager().execute(query, self.id_admin)
+
+        @staticmethod
+        def get_by_id(id_admin) -> 'Admin':
+            query = "SELECT * FROM admin WHERE id_admin = ? "
+            result = ConnectionManager().execute(query, id_admin)
+            res = result.fetchone()
+
+            if res is None:
+                return None
+
+            id, password = res
+            return Admin(id, password)
+
+        def to_json(self) -> dict:
+            return {
+                "id_admin": self.id_admin,
+                "id_password": self.password
+            }
+
+        def __str__(self):
+            return str(self.to_json())
+
+        @staticmethod
+        def from_json(json: dict) -> 'Admin':
+            return Admin(json.get("id_admin", None), json.get("id_password", None))
+
+
+# Player
+    class Player():
+        def __init__(self, id_player: int, name: str, score: str) -> None:
+            self.id_player = id_player
+            self.name = name
+            self.score = score
+
+        def save(self) -> None:
+            if self.id_player is None:
+                query = "INSERT INTO player(name, score) VALUES (?, ?)"
+                result = ConnectionManager().execute(query, self.name, self.score)
+                self.id_player = result.lastrowid
+            else:
+                query = "UPDATE player SET name = ?, score = ? WHERE id_player = ?"
+                ConnectionManager().execute(query, self.name, self.score, self.id_player)
+
+        def delete(self) -> None:
+            query = "DELETE FROM player WHERE id_player = ? "
+            ConnectionManager().execute(query, self.id_player)
+
+        @staticmethod
+        def get_by_id(id_player) -> 'Player':
+            query = "SELECT * FROM player WHERE id_player = ? "
+            result = ConnectionManager().execute(query, id_player)
+            res = result.fetchone()
+
+            if res is None:
+                return None
+
+            id, name, score = res
+            return Player(id, name, score)
+        
+        @staticmethod
+        def get_by_name(name) -> 'Player':
+            query = "SELECT * FROM player WHERE name = ? "
+            result = ConnectionManager().execute(query, name)
+            res = result.fetchone()
+
+            if res is None:
+                return None
+            id, name, score = res
+            return Player(id,name,score)
+
+        def to_json(self) -> dict:
+            return {
+                "id_player": self.id_player,
+                "name": self.name,
+                "score": self.score
+            }
+
+        def __str__(self):
+            return str(self.to_json())
+
+        @staticmethod
+        def from_json(json: dict) -> 'Player':
+            return Player(json.get("id_player", None), json.get("name", None), json.get("score", None))
