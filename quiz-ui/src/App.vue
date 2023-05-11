5fb96d14a6874d7df29a9c6c36de4c435a827809
dasilvar <remi.dasilva@edu.esiee.fr>
2023-05-11 08:59:08 +0200

    refact: all

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -1,33 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-        <div class="container-fluid">
-          <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" to="/new-quiz">New Quiz</RouterLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </header>
-
+  <AppHeader />
   <RouterView />
 </template>
+
+<script>
+import { RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  },
+}
+</script>
 
 <style scoped></style>
