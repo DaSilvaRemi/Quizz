479b9b691616594bf7e7e9ab55d9e191b6ec0fa9
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2023-05-18 13:18:25 +0200

    feat: ajout d'une modale de confirmation de suppression

--- quiz-ui/src/components/ListQuestionsDisplay.vue
+++ quiz-ui/src/components/ListQuestionsDisplay.vue
@@ -24,44 +24,72 @@
                         <i class="bi bi-eye"></i>
                         <i class="bi bi-pencil"></i>
                     </button>
-                    <button class="btn mb-4" type="button" @click="handleClickDeleteQuestion(question.id)">
+                    <button class="btn mb-4" type="button" data-bs-toggle="modal" data-bs-target="#validation-modal">
                         <i class="bi bi-trash"></i>
                     </button>
                 </td>
             </tr>
+            <ValidationModal 
+                titre="Avertissement !"
+                body="Voulez vous supprimer cette question ?"
+                @modal-click-btn-ok="handleClickDeleteQuestion(question.id)"
+            />
         </tbody>
     </table>
+
 </template>
 
 
 <script>
+import ValidationModal from "@/components/ValidationModal.vue"
 import quizApiService from "@/services/QuizApiService.js";
 export default {
     name: "ListQuestionsDisplay",
+    components: {
+        ValidationModal
+    },
+    props: {
+        token: String
+    },
     data() {
         return {
             allQuestions: [],
         };
     },
-    created() {
-        quizApiService
-            .getAllQuestions()
-            .then(response => {
-                if (response.status !== 200) {
-                    const ERROR = `CODE : ${response.status} getAllQuestions`
-                    return Promise.reject(ERROR);
-                }
-                this.allQuestions = [...response.data.questions];
-                console.log(this.allQuestions);
-            });
+    async created() {
+        this.loadAllQuestions()
     },
     methods: {
         handleClickUpdateQuestion(index) {
             this.$router.push(`/questions/${index}`);
         },
-        handleClickDeleteQuestion(index) {
-            quizApiService.deleteQuestionById(index);
+        async handleClickDeleteQuestion(index) {
+            quizApiService.deleteQuestionById(index, this.token)
+                .then(response => {
+                    if (response.status !== 204) {
+                        const ERROR = `CODE : ${response.status} postQuestion`
+                        return Promise.reject(ERROR);
+                    }
+
+                    this.loadAllQuestions();
+                }
+                ).catch((error) => {
+                    console.error(error);
+                });
         },
+        async loadAllQuestions() {
+            quizApiService
+                .getAllQuestions()
+                .then(response => {
+                    if (response.status !== 200) {
+                        const ERROR = `CODE : ${response.status} getAllQuestions`
+                        return Promise.reject(ERROR);
+                    }
+                    this.allQuestions = [...response.data.questions];
+                }).catch((error) => {
+                    console.error(error);
+                });
+        }
     },
 }
 </script>
---
+++ quiz-ui/src/components/ValidationModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="modal" id="validation-modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ titre }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>{{ body }}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="handleClickButtonOk" >Confirmer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ValidationModal",
+    props: {
+        titre: String,
+        body: String,
+    },
+    methods:{
+        handleClickButtonOk(){
+            this.$emit("modal-click-btn-ok");
+        }
+    }
+}
+</script>
